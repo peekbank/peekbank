559d4085112fef3bae550769712d1603a5f26e35
Sarp Uner <sarpim@gmail.com>
2020-08-28 17:04:51 -0400

    Downloading files work. Adding hashed json

--- db/management/commands/download_osf.py
+++ db/management/commands/download_osf.py
@@ -4,14 +4,20 @@ import osfclient as osf
 import requests
 import os.path
 import json
+import errno
+from pathlib import Path
 
 
 # TODO: Add pagination support.
 # TODO: Turn into a django management command!
 # TODO: Just very, very, very ugly/shotgun code here :(
 # TODO: Add error handling/
+# TODO: Multiprocessing ??
+# TODO: Fix the awful directory/paths (replace with tempdir/files?)
 
 BASE_OSF_URL = 'https://api.osf.io/v2/nodes/pr6wu/files/osfstorage/'
+
+TOP_LEVEL_DIRECTORY = os.path.abspath(os.path.join(__file__, "../../../../data"))
 
 
 class Command(BaseCommand):
@@ -27,13 +33,41 @@ class Command(BaseCommand):
 
     def find_processed_folder(self, folder):
         # The empty string in os.path.join is to add a path separator to the end. 
-        r = requests.get(os.path.join(base_url, folder['id'], ''))
+        r = requests.get(os.path.join(BASE_OSF_URL, folder['id'], ''))
         response_dict = json.loads(r.content)
         for item in response_dict['data']:
             if item['attributes']['name'] == 'processed_data':
                 return item
 
     def download_processed_data(self, folder):
+        r = requests.get(os.path.join(BASE_OSF_URL, folder['id'], ''))
+        response_dict = json.loads(r.content)
+        for item in response_dict['data']:
+            if item:
+                file_name = item['attributes']['name']
+                materialized_path = item['attributes']['materialized_path']
+                file_hashes = item['attributes']['extra']['hashes']
+
+                # The real path at which this file will be saved
+                file_path = os.path.join(TOP_LEVEL_DIRECTORY, *materialized_path.split('/'))
+
+                if not os.path.exists(os.path.dirname(file_path)):
+                    try:
+                        os.makedirs(os.path.dirname(file_path))
+                    except OSError as exc:
+                        if exc.errno != errno.EEXIST:
+                            raise
+
+                r = requests.get(item['links']['download'])
+                with open(file_path, 'wb') as fd:
+                    fd.write(r.content)
+
+
+
+
+
+        
+        return
 
 
         
@@ -51,10 +85,24 @@ class Command(BaseCommand):
 
 
 if __name__ == "__main__":
+
+    try:
+        os.mkdir(TOP_LEVEL_DIRECTORY)
+
+    except FileExistsError:
+        print('Data directory already exists')
+
+
+
     c = Command()
     folders = c.gather_folders()
+
+
     for folder in folders:
-        check_and_download_folder(folder)
+        processed = c.find_processed_folder(folder)
+        c.download_processed_data(processed)
+
+
 
 
 
