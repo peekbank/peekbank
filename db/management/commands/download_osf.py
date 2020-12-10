3d8e74e2a775925f023ba03ad2ece53ca96b12f6
Sarp Uner <sarpim@gmail.com>
2020-12-10 14:37:38 -0500

    Teamwork makes the dream wooooooooork!!!!!!!!

--- db/management/commands/download_osf.py
+++ db/management/commands/download_osf.py
@@ -12,20 +12,22 @@ from django.conf import settings
 from pdb import set_trace as st
 
 
-# TODO: Add pagination support.
-# TODO: Turn into a django management command!
+# TODO: Add pagination support. WE DID IT HAHAHAHAHAHAHAHAHAAH
+# TODO: Turn into a django management command! DONE ATAHAHFASHFAHSDHAHASHFASDFAHDSFH
 # TODO: Just very, very, very ugly/shotgun code here :(
 # TODO: Add error handling/
 # TODO: Multiprocessing ??
 # TODO: Fix the awful directory/paths (replace with tempdir/files?)
+# TODO: add tqdm for progress
+# TODO: Graceful interrupt handling
 
+
+# Visit this url to see what the json response will look like.
 BASE_OSF_URL = 'https://api.osf.io/v2/nodes/pr6wu/files/osfstorage/'
 
 
-#TOP_LEVEL_DIRECTORY = os.path.abspath(os.path.join(settings.BASE_DIR, 'data'))
 
-TOP_LEVEL_DIRECTORY = os.path.abspath("/home/ubuntu/peekbank_data_osf")
-#TOP_LEVEL_DIRECTORY = os.path.abspath(os.path.join(__file__, "../../../../data"))
+TOP_LEVEL_DIRECTORY = os.path.abspath("/home/sarp/ubuntu/peekbank_data_osf")
 
 
 class Command(BaseCommand):
@@ -36,12 +38,22 @@ class Command(BaseCommand):
     def gather_folders(self):
         print('Gathering folders....')
         payload = {'filter[kind]': 'folder'}
-        r = requests.get(BASE_OSF_URL, params = payload)
-        response = json.loads(r.content.decode('utf-8'))
-        folders = [folder for folder in response['data']]
+        folders = []
+        current_link = self.collect_page(BASE_OSF_URL, payload, folders)
+        while(current_link != None):
+            current_link = self.collect_page(current_link, payload, folders)
+
         print("Found the following folders:")
-        print(folders)
+        print([folder['attributes']['materialized_path'] for folder in folders])
+
         return folders
+
+    def collect_page(self, url, payload, folders):
+        r = requests.get(url, params = payload)
+        response = json.loads(r.content.decode('utf-8'))
+        folders.extend([folder for folder in response['data']])
+        return response['links']['next'] 
+
 
     def find_processed_folder(self, folder):
         print('Finding processed folders...')
@@ -54,16 +66,14 @@ class Command(BaseCommand):
 
     def download_processed_data(self, folder):
         print('Downloading processed data...')
+        print('Downloading folder: {}'.format(folder['attributes']['materialized_path']))
         r = requests.get(os.path.join(BASE_OSF_URL, folder['id'], ''))
         response_dict = json.loads(r.content.decode('utf-8'))
         for item in response_dict['data']:
             if item:
                 file_name = item['attributes']['name']
                 materialized_path = item['attributes']['materialized_path']
-                #file_hashes = item['attributes']['extra']['hashes']
 
-
-                # The real path at which this file will be saved
                 file_path = os.path.join(TOP_LEVEL_DIRECTORY, *materialized_path.split('/'))
 
                 if not os.path.exists(os.path.dirname(file_path)):
@@ -76,14 +86,8 @@ class Command(BaseCommand):
                 r = requests.get(item['links']['download'])
                 with open(file_path, 'wb') as fd:
                     fd.write(r.content)
-
-                # hash_file = Path(file_path).with_suffix('.json')
-                # with open(hash_file, 'w') as fd:
-                #     json.dump(file_hashes, fd)
         
         return
-
-#r = requests.get(my_dict['data'][0]['relationships']['files']['links']['related']['href'])
 
     def handle(self, *args, **options):  
         print('Handling...')      
@@ -100,10 +104,15 @@ class Command(BaseCommand):
         folders = self.gather_folders()
 
 
+        processed_list = []
         for folder in folders:        
             processed = self.find_processed_folder(folder)
+            processed_list.append(processed)
             if processed:
                 self.download_processed_data(processed)
+        print('Found these processed folders')
+        print(processed_list)
+
 
 
 
