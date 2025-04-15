4d819dc99a9f52b9cda965ae4d9c430b44f4efdb
Adrian Steffan <adrian.steffan@hotmail.de>
2025-04-15 21:21:40 +0200

    fix print

--- db/management/commands/populate.py
+++ db/management/commands/populate.py
@@ -539,10 +539,6 @@ class Command(BaseCommand):
         
         if validate_only or keep:
             self.stdout.write(self.style.SUCCESS("Keeping existing database..."))
-        else:
-            self.stdout.write(
-                self.style.SUCCESS("Keeping existing database as requested...")
-            )
            
             sql_script = f"""
     CREATE USER IF NOT EXISTS '{db_user}'@'%' IDENTIFIED BY '{db_password}';
